ce4344c1d4c955616fafd517c0b64be6fc95ef67
uyiekpen <71497866+uyiekpen@users.noreply.github.com>
2020-12-17 07:09:28 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import './App.css';
-import Podcast from './Podcast/Podcast';
+import Header from './Components/Header/Header';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Sound from './Components/Linkpage/Sound';
+import About from './Components/NavLink/About';
+import Episode from './Components/NavLink/Episode';
+import Contact from './Components/NavLink/Contact';
 
 function App() {
   return (
-    <div>
-      <Podcast />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Header} />
+        <Route path="/sound" exact component={Sound} />
+        <Route path="/about" exact component={About} />
+        <Route path="/episode" exact component={Episode} />
+        <Route path="/contact" exact component={Contact} />
+
+      </Switch>
+    </Router>
   );
 }
 
